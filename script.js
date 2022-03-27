bbfdf00b1eee578840a2baf13214ea59294ef25f
Carlos Fontanez <charliefontanezis@gmail.com>
2022-03-27 17:43:18 -0400

    added styling to textarea to show past present and future timeblocks.

--- script.js
+++ script.js
@@ -1,28 +1,43 @@
 var container = document.querySelector(".container");
 var headerEl = document.querySelector("header");
 var dayDisplayEl = document.querySelector("#currentDay");
+var currentTime = moment().hour();
 var btns = document.querySelectorAll(".saveBtn");
-var blocks = JSON.parse(window.localStorage.getItem('blockData')) || [];
+var blocks = [];
 
-if (blocks.length == 0) {
-  for (let i = 1; i < 10; i++) {
-    let x = document.getElementById("1" * i);
-    blocks.push(x);
-  }
-}
+currentTime = 13;
+// if (blocks.length == 0) {
+//   for (let i = 1; i < 10; i++) {
+//     let x = document.getElementById("1" * i);
+//     blocks.push(x);
+//   }
+// }
 
 function onLoad() {
   dayDisplayEl.innerHTML = moment().format('dddd, MMMM Do');
   loadBlocks();
+
+  blocks.forEach(function(data) {
+    data.children[1].classList.add("past");
+  });
+  for (var i = 0; i < 9; i++) {
+    if (currentTime == parseInt(blocks[i].id)) {
+      blocks[i].children[1].classList.add("present")
+    }
+    else if (currentTime < parseInt(blocks[i].id)){
+      blocks[i].children[1].classList.remove("past");
+      blocks[i].children[1].classList.add("future");
+    }
+  }
 }
 
 function loadBlocks() {
-  for (let i = 1; i < 10; i++) {
-    let y = document.getElementById(i.toString());
-    let children = y.children;
-    console.log(children[1]);
-    console.log(y);
-    children[1].value = localStorage.getItem(i);
+  for (let i = 0; i < 9; i++) {
+    let y = document.getElementById((i + 9).toString());
+    blocks.push(y);
+    // let children = y.children;
+    console.log(blocks[i].children[1]);
+    blocks[i].children[1].value = localStorage.getItem(i);
   }
 }
 
@@ -35,15 +50,7 @@ function save() {
 
 onLoad();
 
-
 btns.forEach(btn => {
   btn.addEventListener("click", save);
 });
-
-// for (let i = 1; i < 10; i++) {
-//   var blockEl = document.getElementById("#" + i.toString())
-//   blockEl.addEventListener("click", save);
-// }
-
-
 
