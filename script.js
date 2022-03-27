7bf8dd0bf5ca39585199b3157345eabd5ac129fb
Carlos Fontanez <charliefontanezis@gmail.com>
2022-03-27 15:02:10 -0400

    saved time blocks in localStorage

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 var container = document.querySelector(".container");
 var headerEl = document.querySelector("header");
 var dayDisplayEl = document.querySelector("#currentDay");
-dayDisplayEl.innerHTML = moment().format('dddd, MMMM Do');
-
+var btns = document.querySelectorAll(".saveBtn");
 var blocks = JSON.parse(window.localStorage.getItem('blockData')) || [];
 
 if (blocks.length == 0) {
@@ -12,15 +11,39 @@ if (blocks.length == 0) {
   }
 }
 
-function save() {
-  console.dir(this.previousSibling);
+function onLoad() {
+  dayDisplayEl.innerHTML = moment().format('dddd, MMMM Do');
+  loadBlocks();
+}
+
+function loadBlocks() {
+  for (let i = 1; i < 10; i++) {
+    let y = document.getElementById(i.toString());
+    let children = y.children;
+    console.log(children[1]);
+    console.log(y);
+    children[1].value = localStorage.getItem(i);
+  }
 }
 
 
+function save() {
+  var textContent = this.previousElementSibling.value;
+  var blockElById = this.parentElement.id;
+  localStorage.setItem(blockElById, textContent);
+}
 
-document.querySelectorAll(".saveBtn").forEach(saveBtn => {
-  saveBtn.addEventListener('click', save);
+onLoad();
+
+
+btns.forEach(btn => {
+  btn.addEventListener("click", save);
 });
+
+// for (let i = 1; i < 10; i++) {
+//   var blockEl = document.getElementById("#" + i.toString())
+//   blockEl.addEventListener("click", save);
+// }
 
 
 
