75d760fd18b77c9b37023441411fc2054cae0102
Carlos Fontanez <charliefontanezis@gmail.com>
2022-03-27 11:38:38 -0400

    set up html and added current date with moment

---
+++ script.js
@@ -0,0 +1,19 @@
+var container = document.querySelector(".container");
+var headerEl = document.querySelector("header");
+var dayDisplayEl = document.querySelector("#currentDay");
+dayDisplayEl.innerHTML = moment().format('dddd, MMMM Do');
+
+var blocks = JSON.parse(window.localStorage.getItem('blockData')) || [];
+
+if (blocks.length == 0) {
+  for (let i = 1; i < 10; i++) {
+    let x = document.getElementById("1" * i);
+    blocks.push(x);
+  }
+}
+
+
+
+
+
+
